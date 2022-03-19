9a6bf19a620886883b676a83434040b82a64d56b
SergReva <SergReva@users.noreply.github.com>
2022-03-19 11:35:45 +0200

    Day 1 update

--- js/tabs.js
+++ js/tabs.js
@@ -1,6 +1,8 @@
 const tabButtons = document.querySelectorAll('.design-list__item'),
   tabDescriptions = document.querySelectorAll('.design__descr'),
-  tabImages = document.querySelectorAll('.design-images');
+  tabImages = document.querySelectorAll('.design-images'),
+  tabMainImage = document.querySelectorAll('.design-block__img'),
+  tabTitle = document.querySelectorAll('.design__title');
 
 const changeContent = (array, value) => {
   array.forEach((e) => {
@@ -10,21 +12,33 @@ const changeContent = (array, value) => {
       e.classList.add('hidden');
     }
   })
-}
+};
+
+tabTitle.forEach((title)=>{
+  if (title.classList.contains('hidden')) {
+    title.dataset.tabsField = 'body';
+  } else {
+    title.dataset.tabsField = 'interior';
+    
+  }
+})
 
 tabButtons.forEach((tabButton) => {
   tabButton.addEventListener('click', (e) => {
     const dataValue = tabButton.dataset.tabsHandler;
-
+    
     changeContent(tabDescriptions, dataValue);
     changeContent(tabImages, dataValue);
+    changeContent(tabMainImage, dataValue);
+    changeContent(tabTitle, dataValue);
     
     tabButtons.forEach((btn) => {
       if (btn === e.target) {
         btn.classList.add('design-list__item_active');
-        document.title = btn.innerHTML + ' - Mercedes Benz';
+        document.title = tabTitle[1].innerText + ' - Mercedes Benz';
       } else {
         btn.classList.remove('design-list__item_active');
+        document.title = tabTitle[0].innerText + ' - Mercedes Benz';
       }
     })
   })
